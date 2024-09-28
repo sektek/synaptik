b8fb5701d42b250ab52d190c09affd38124463fb
Edward Kelly <eddie@sektek.net>
2024-09-28 16:15:41 +0000

    feat: added EventBuilder

---
+++ src/event-builder.spec.ts
@@ -0,0 +1,160 @@
+import { expect } from 'chai';
+
+import { Event } from './types/event.js';
+import { EventBuilder } from './event-builder.js';
+
+type TestEventType = 'TestEvent';
+type TestEventData = {
+  value: string;
+};
+type TestEvent = Event & {
+  type: TestEventType;
+  data: TestEventData;
+};
+
+describe('EventBuilder', function () {
+  describe('without type', function () {
+    it('should create an event', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.create();
+      expect(event.type).to.equal('Event');
+    });
+
+    it('should create an event with a uuid id', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.create();
+      expect(event.id).to.match(
+        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i,
+      );
+    });
+
+    it('should create an event with data', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.create({ value: 'test' });
+      expect(event.data?.value).to.equal('test');
+    });
+
+    it('should create data from a provided function', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.create({ value: () => 'test' });
+      expect(event.data?.value).to.equal('test');
+    });
+
+    it('should permit data to be provided in the constructor', function () {
+      const eventBuilder = new EventBuilder({ data: { value: 'test' } });
+      const event = eventBuilder.create();
+      expect(event.data?.value).to.equal('test');
+    });
+
+    it('should override data provided in the constructor', function () {
+      const eventBuilder = new EventBuilder({ data: { value: 'test' } });
+      const event = eventBuilder.create({ value: 'test2' });
+      expect(event.data?.value).to.equal('test2');
+    });
+
+    it('should permit headers to be provided in the constructor', function () {
+      const eventBuilder = new EventBuilder({ headers: { id: 'test' } });
+      const event = eventBuilder.create();
+      expect(event.id).to.equal('test');
+    });
+  });
+
+  describe('with type', function () {
+    it('should create an event', function () {
+      const eventBuilder = new EventBuilder<TestEvent>({ type: 'TestEvent' });
+      const event = eventBuilder.create({ value: 'test' });
+      expect(event.type).to.equal('TestEvent');
+      expect(event.data.value).to.equal('test');
+    });
+
+    it('should permit data to be provided in the constructor', function () {
+      const eventBuilder = new EventBuilder<TestEvent>({
+        type: 'TestEvent',
+        data: { value: 'test' },
+      });
+      const event = eventBuilder.create();
+      expect(event.data.value).to.equal('test');
+    });
+
+    it('should permit data to be provided in the constructor and create', function () {
+      const eventBuilder = new EventBuilder<TestEvent>({ type: 'TestEvent' });
+      const event = eventBuilder.create({ value: 'test' });
+      expect(event.data.value).to.equal('test');
+    });
+  });
+
+  describe('with() method', function () {
+    it('should create an event', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.with({ data: { value: 'test' } }).create();
+      expect(event.data.value).to.equal('test');
+    });
+
+    it('should override data', function () {
+      const eventBuilder = new EventBuilder({ data: { value: 'test' } });
+      const event = eventBuilder.with({ data: { value: 'test2' } }).create();
+      expect(event.data.value).to.equal('test2');
+    });
+  });
+
+  describe('from() method', function () {
+    it('should copy id from event to parentId', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder.create();
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.parentId).to.equal(event.id);
+    });
+
+    it('should copy parentId from event to parentId', function () {
+      const eventBuilder = new EventBuilder();
+      const event = eventBuilder
+        .with({ headers: { parentId: 'test' } })
+        .create();
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.parentId).to.equal('test');
+    });
+
+    it('should copy data from event', function () {
+      const eventBuilder = new EventBuilder<TestEvent>({
+        copyableData: ['value'],
+      });
+      const event = eventBuilder.create({ value: 'test' });
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.data.value).to.equal('test');
+    });
+
+    it('should allow any copyable data for non-typed events', function () {
+      const eventBuilder = new EventBuilder({
+        copyableData: ['value'],
+      });
+      const event = eventBuilder.create({ value: 'test' });
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.data.value).to.equal('test');
+    });
+
+    it('should override data from event', function () {
+      const eventBuilder = new EventBuilder<TestEvent>({
+        copyableData: ['value', 'blah'],
+      });
+      const event = eventBuilder.create({ value: 'test' });
+      const event2 = eventBuilder.from(event).create({ value: 'test2' });
+      expect(event2.data.value).to.equal('test2');
+    });
+
+    it('should copy data from event regardless of type', function () {
+      const event = new EventBuilder().create({ value: 'test' });
+      const eventBuilder = new EventBuilder<TestEvent>({
+        copyableData: ['value'],
+      });
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.data.value).to.equal('test');
+    });
+
+    it('should copy id to parentId from event regardless of type', function () {
+      const event = new EventBuilder().create();
+      const eventBuilder = new EventBuilder<TestEvent>();
+      const event2 = eventBuilder.from(event).create();
+      expect(event2.parentId).to.equal(event.id);
+    });
+  });
+});
---
+++ src/event-builder.ts
@@ -0,0 +1,184 @@
+import _ from 'lodash';
+
+import { Event } from './types/event.js';
+import { TransformToBuildOpts } from './util/transform-to-build-options.js';
+import { isBrowser } from './util/is-browser.js';
+
+let randomUUID: () => string;
+
+if (isBrowser()) {
+  // eslint-disable-next-line no-undef
+  randomUUID = window.crypto.randomUUID;
+} else {
+  randomUUID = (await import('crypto')).randomUUID;
+}
+
+type EventHeadersBuildOpts<T extends Event = Event> = TransformToBuildOpts<
+  Omit<T, 'data' | 'type'>
+>;
+
+type EventDataBuildOpts<T extends Event = Event> = TransformToBuildOpts<
+  T['data']
+>;
+
+export type EventBuilderOptions<T extends Event = Event> = {
+  data?: EventDataBuildOpts<T>;
+  copyableData?: string[];
+  headers?: EventHeadersBuildOpts<T>;
+  type?: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isFunction = (value: unknown): value is () => any =>
+  typeof value === 'function';
+
+/**
+ * A builder for creating events.
+ *
+ * @template T The type of event to build.
+ */
+export class EventBuilder<T extends Event = Event> {
+  #data: EventDataBuildOpts<T>;
+  #headers: EventHeadersBuildOpts<T>;
+  #type: T['type'];
+  #copyableData: string[];
+
+  /**
+   * Creates a new EventBuilder with the provided options.
+   *
+   * @param opts The options to use when creating the event.
+   * @param opts.copyableData The data attributes to copy from an existing
+   *                          event when calling the from() method.
+   * @param opts.data The data attributes to use when creating an event.
+   *                  Will accept both a value or a function that returns
+   *                  a value.
+   * @param opts.headers The headers to use when creating an event. As with
+   *                     the data attribute, will accept both a value or a
+   *                     function that returns a value. The id header will
+   *                     default to a random UUID.
+   * @param opts.type The type of event to create. Will default to 'Event'.
+   */
+  constructor(opts: EventBuilderOptions<T> = {}) {
+    this.#data = opts.data ?? {};
+    this.#type = (opts.type ?? 'Event') as T['type'];
+    this.#headers = {
+      id: randomUUID,
+      ...(opts.headers ?? {}),
+    } as EventHeadersBuildOpts<T>;
+    this.#copyableData = opts.copyableData ?? [];
+  }
+
+  /**
+   * Creates a new EventBuilder with the provided options merged with the
+   * current options.
+   *
+   * @param opts The options to merge with the current options.
+   * @returns A new EventBuilder with the merged options.
+   */
+  with(opts: EventBuilderOptions<T>): EventBuilder<T> {
+    return new EventBuilder<T>(_.merge(this.eventBuilderOptions, opts));
+  }
+
+  /**
+   * Creates a new EventBuilder with the data from the provided event
+   * merged with the current options. Will copy the parentId or event id to
+   * the newly created event. Will also copy any data that is in the
+   * copyableData array of the builder.
+   *
+   * @param event The event to copy data from.
+   * @returns A new EventBuilder with the merged data.
+   */
+  from(event: Event): EventBuilder<T> {
+    return new EventBuilder<T>(
+      _.merge(this.eventBuilderOptions, this.#optionsFromEvent(event)),
+    );
+  }
+
+  #optionsFromEvent(event: Event): EventBuilderOptions<T> {
+    return {
+      data: this.#dataFromEvent(event),
+      headers: this.#headersFromEvent(event),
+    };
+  }
+
+  #dataFromEvent(event: Event): EventDataBuildOpts<T> {
+    const result: Record<string, unknown> = {};
+
+    this.#copyableData.reduce((acc, key: string) => {
+      if (event.data[key] !== undefined) {
+        acc[key] = event.data[key];
+      }
+      return acc;
+    }, result);
+
+    return result as EventDataBuildOpts<T>;
+  }
+
+  #headersFromEvent(event: Event): EventHeadersBuildOpts<T> {
+    const result: Record<string, unknown> = {};
+
+    if (!this.#headers.parentId) {
+      result.parentId = event.id;
+    }
+
+    return result as EventHeadersBuildOpts<T>;
+  }
+
+  /**
+   * Create an event using the data included within the builder
+   * and allowing additional attributes to be provided to override
+   * the builder's data. Only event data attributes can be overridden.
+   *
+   * @param createOps The data to use when creating the event.
+   * @returns The created event.
+   */
+  create(createOps: EventDataBuildOpts<T> = {}): T {
+    const result: Record<string, unknown> = {
+      ...this.#renderHeaders(),
+      data: this.#renderData(_.merge(this.#data, createOps)),
+    };
+
+    return result as T;
+  }
+
+  #renderHeaders(): Partial<T> {
+    const result: Record<string, unknown> = {
+      type: this.#type ?? 'Event',
+    };
+    Object.keys(this.#headers || {}).reduce(
+      (acc: Record<string, unknown>, key: string) => {
+        if (this.#headers?.[key] !== undefined) {
+          acc[key] = isFunction(this.#headers?.[key])
+            ? (this.#headers[key] as () => unknown)()
+            : this.#headers?.[key];
+        }
+        return acc;
+      },
+      result,
+    );
+
+    return result as Partial<T>;
+  }
+
+  #renderData(data: EventDataBuildOpts<T>): Partial<T['data']> {
+    const result: Record<string, unknown> = {};
+
+    Object.keys(data || {}).reduce((acc, key: string) => {
+      if (data?.[key] !== undefined) {
+        acc[key] = isFunction(data?.[key]) ? data[key]() : data?.[key];
+      }
+      return acc;
+    }, result);
+
+    return result as Partial<T['data']>;
+  }
+
+  private get eventBuilderOptions(): EventBuilderOptions<T> {
+    return {
+      copyableData: this.#copyableData,
+      data: this.#data,
+      headers: this.#headers,
+      type: this.#type,
+    };
+  }
+}
