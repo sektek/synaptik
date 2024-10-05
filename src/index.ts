34b242f8b69c491bf6ef3be9a1c3fc9c8c1e95bd
Edward Kelly <eddie@sektek.net>
2024-10-05 12:58:31 +0000

    feat: Add export for processing-channel module

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './null-handler.js';
 export * from './util/index.js';
 export * from './null-channel.js';
 export * from './null-handler.js';
+export * from './processing-channel.js';
 export * from './promise-channel.js';
 export * from './route-store.js';
 export * from './single-use-route-store.js';
