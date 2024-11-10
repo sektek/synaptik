854aa86179238aeb29b83b88126172ee87a73e21
Edward Kelly <eddie@sektek.net>
2024-11-10 15:06:59 +0000

    fix: provide default opts for AbstractEventService constructor

--- src/abstract-event-service.ts
+++ src/abstract-event-service.ts
@@ -24,7 +24,7 @@ export abstract class AbstractEventService
 {
   #name: string;
 
-  constructor(opts: EventServiceOptions) {
+  constructor(opts: EventServiceOptions = {}) {
     super();
     this.#name = opts.name ?? generateName(this.constructor.name);
   }
