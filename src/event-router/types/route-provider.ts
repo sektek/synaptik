e55a815f8da2f4d9ee4c054df721fb2cb03a1099
Edward Kelly <eddie@sektek.net>
2024-11-10 15:38:02 +0000

    chore: modified RouteProvider to inherit from EventBaseProvider

--- src/event-router/types/route-provider.ts
+++ src/event-router/types/route-provider.ts
@@ -1,5 +1,7 @@
 import {
   Event,
+  EventBasedProvider,
+  EventBasedProviderFn,
   EventEndpointComponent,
   EventHandlerFn,
 } from '../../types/index.js';
@@ -14,13 +16,13 @@ export type Route<T extends Event = Event> = EventEndpointComponent<T>;
 /**
  * A function that provides one or more event handlers for a given event.
  */
-export type RouteProviderFn<T extends Event = Event> = (
-  event: T,
-) => Promise<RouteFn<T> | RouteFn<T>[]>;
+export type RouteProviderFn<T extends Event = Event> = EventBasedProviderFn<
+  T,
+  RouteFn<T> | RouteFn<T>[]
+>;
 
 /**
  * A class that provides one or more event handlers for a given event.
  */
-export interface RouteProvider<T extends Event = Event> {
-  get: RouteProviderFn<T>;
-}
+export interface RouteProvider<T extends Event = Event>
+  extends EventBasedProvider<T, RouteFn<T> | RouteFn<T>[]> {}
