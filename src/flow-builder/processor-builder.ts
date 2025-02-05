1d78fd1dfb5aff0a1d83ebf4568d62d52afd5367
Edward Kelly <eddie@sektek.net>
2025-02-05 15:45:39 +0000

    WIP

---
+++ src/flow-builder/processor-builder.ts
@@ -0,0 +1,38 @@
+// import { BuilderOptions, builderRender } from '@sektek/utility-belt';
+
+// import {
+//   Event,
+//   EventEndpointComponent,
+//   EventProcessorComponent,
+// } from '../types/index.js';
+// import {
+//   ProcessingChannel,
+//   ProcessingChannelOptions,
+// } from '../channels/processing-channel.js';
+
+// export class ProcessorBuilder<T extends Event = Event, R extends Event = T> {
+//   #opts: Partial<ProcessingChannelOptions<T, R>> = {};
+//   #handler?: EventEndpointComponent<R>;
+
+//   constructor(opts: Omit<ProcessingChannelOptions<T, R>, 'handler'>) {
+//     this.#opts = opts;
+//   }
+
+//   processor(processor: EventProcessorComponent<T, R>): this {
+//     this.#opts.processor = processor;
+//     return this;
+//   }
+
+//   handler(handler: EventEndpointComponent<R>): this {
+//     this.#handler = handler;
+//     return this;
+//   }
+
+//   create(): ProcessingChannelEventEmitter<T, R> {
+//     if (!this.#handler) {
+//       throw new Error('Handler is required');
+//     }
+
+//     return new ProcessingChannel({ ...this.#opts, handler: this.#handler });
+//   }
+// }
