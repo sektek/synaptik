8278be2a275be521cda88c59434458cd348296d5
Edward Kelly <eddie@sektek.net>
2025-02-05 07:33:38 -0800

    feat: enhance getEventHandlerComponent with support for return type (#18)

--- src/util/get-event-handler-component.ts
+++ src/util/get-event-handler-component.ts
@@ -1,14 +1,32 @@
 import { getComponent } from '@sektek/utility-belt';
 
-import { Event, EventHandler, EventHandlerFn } from '../types/index.js';
+import {
+  Event,
+  EventChannel,
+  EventHandler,
+  EventHandlerFn,
+  EventHandlerReturnType,
+  EventProcessor,
+} from '../types/index.js';
 
-export const getEventHandlerComponent = <T extends Event = Event>(
+type DefaultHandlerType<
+  T extends Event = Event,
+  R extends EventHandlerReturnType = unknown,
+> =
+  | EventHandlerFn<T, R>
+  | (R extends Event ? EventProcessor<T, R> : never)
+  | (R extends void | unknown ? EventHandler<T> | EventChannel<T> : never);
+
+export const getEventHandlerComponent = <
+  T extends Event = Event,
+  R extends EventHandlerReturnType = unknown,
+>(
   obj: unknown | null | undefined,
-  defaultHandler?: EventHandler<T> | EventHandlerFn<T>,
-): EventHandlerFn<T> => {
+  defaultHandler?: DefaultHandlerType<T, R>,
+): EventHandlerFn<T, R> => {
   return getComponent(
     obj,
     ['send', 'process', 'handle'],
     defaultHandler,
-  ) as EventHandlerFn<T>;
+  ) as EventHandlerFn<T, R>;
 };
