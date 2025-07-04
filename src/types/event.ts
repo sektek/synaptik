1220db065eeec64ad3334b64f9ecd324f4162f58
Edward Kelly <eddie@sektek.net>
2025-07-04 21:26:34 +0000

    chore: refactor EventData type and improve documentation for Event types

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,5 +1,9 @@
-export type EventData = Record<string, unknown>;
+type EventData = Record<string, unknown>;
 
+/**
+ * Options for building event data.
+ * @template T The type of the event data, defaults to 'Event'
+ */
 export type EventHeaders = {
   id: string;
   parentId?: string;
@@ -7,8 +11,14 @@ export type EventHeaders = {
   type: string;
 };
 
-export type Event = EventHeaders & {
-  data: EventData;
+/**
+ * Represents an event in the system.
+ * @template T The type of the event, defaults to 'Event'.
+ * @template D The data associated with the event, defaults to an empty object.
+ * @property {string} id - Unique identifier for the event.
+ */
+export type Event<T = EventData> = EventHeaders & {
+  data: T;
 };
 
 export const EmptyEvent: Event = {
