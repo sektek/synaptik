56a386039287f0a82d0ae9a0de583ebd463eb00c
Edward Kelly <eddie@sektek.net>
2025-07-04 21:35:54 +0000

    feat: refactor HttpChannel and HttpProcessor to use HttpOperator under the hood

--- src/http/http-channel.ts
+++ src/http/http-channel.ts
@@ -1,16 +1,84 @@
-import { Event, EventChannel } from '../types/index.js';
-import { HttpEventService } from './types/http-event-service.js';
-import { SimpleHttpEventService } from './simple-http-event-service.js';
+import { HttpOperator, HttpOperatorOptions } from '@sektek/utility-belt';
 
+import {
+  AbstractEventService,
+  EventServiceOptions,
+} from '../abstract-event-service.js';
+import { Event, EventChannel } from '../types/index.js';
+
+import { HttpEventService } from './types/http-event-service.js';
+
+/**
+ * Options for the HttpChannel.
+ *
+ * @template T The type of event to send.
+ * @property {HttpOperator<T>} [httpOperator] An optional HttpOperator instance to use.
+ */
+export type HttpChannelOptions<T extends Event = Event> = EventServiceOptions &
+  HttpOperatorOptions<T> & {
+    httpOperator?: HttpOperator<T>;
+  };
+
+/**
+ * A channel for sending events over HTTP.
+ * This class uses an HttpOperator to perform HTTP requests based on the
+ * provided event.
+ *
+ * @template T The type of event to send.
+ */
 export class HttpChannel<T extends Event = Event>
-  extends SimpleHttpEventService<T>
+  extends AbstractEventService
   implements EventChannel<T>, HttpEventService<T>
 {
+  #httpOperator: HttpOperator<T>;
+
+  /**
+   * Creates an instance of HttpChannel.
+   *
+   * @param {HttpChannelOptions<T>} [opts] The options for the channel.
+   * @throws {Error} If neither httpOperator, url, nor urlProvider is provided.
+   */
+  constructor(opts: HttpChannelOptions<T> = {}) {
+    super(opts);
+
+    if (!opts.httpOperator && !opts.url && !opts.urlProvider) {
+      throw new Error(
+        'HttpChannel requires either an httpOperator, url, or urlProvider to be provided.',
+      );
+    }
+
+    this.#httpOperator =
+      opts.httpOperator ??
+      new HttpOperator({
+        method: 'POST',
+        ...opts,
+      });
+
+    this.#httpOperator.on('request:received', (event, request) => {
+      this.emit('request:received', event, request);
+    });
+    this.#httpOperator.on('request:error', (event, request, error) => {
+      this.emit('request:error', event, request, error);
+    });
+    this.#httpOperator.on('response:received', (event, response) => {
+      this.emit('response:received', event, response);
+    });
+    this.#httpOperator.on('response:error', (event, response, error) => {
+      this.emit('response:error', event, response, error);
+    });
+  }
+
+  /**
+   * Sends an event over HTTP using the configured HttpOperator.
+   *
+   * @param {T} event The event to send.
+   * @returns {Promise<void>} A promise that resolves when the event is sent.
+   */
   async send(event: T): Promise<void> {
     this.emit('event:received', event);
 
     try {
-      await this.perform(event);
+      await this.#httpOperator.perform(event);
 
       this.emit('event:delivered', event);
     } catch (error) {
