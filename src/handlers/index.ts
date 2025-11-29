89476c5d4a41c127abf2170eaecf23ca403de822
Edward Kelly <eddie@sektek.net>
2025-11-29 06:57:37 -0800

    feat: MutatorHandler (#27)

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,1 +1,2 @@
+export * from './mutator-handler.js';
 export * from './null-handler.js';
---
+++ src/handlers/mutator-handler.ts
@@ -0,0 +1,80 @@
+import {
+  MutatorComponent,
+  MutatorFn,
+  ProviderComponent,
+  ProviderFn,
+  getComponent,
+} from '@sektek/utility-belt';
+
+import {
+  AbstractEventService,
+  EventServiceOptions,
+} from '../abstract-event-service.js';
+
+import { Event } from '../types/event.js';
+import { EventHandler } from '../types/event-handler.js';
+
+/**
+ * Options for the MutatorHandler constructor.
+ */
+export type MutatorHandlerOptions<
+  T extends Event = Event,
+  K = string,
+  V = T,
+> = EventServiceOptions & {
+  /** The mutator component used to set values. */
+  mutator: MutatorComponent<V, K>;
+  /**
+   * The provider component used to extract keys from events.
+   * If not provided, defaults to a provider that returns the event's id as the key.
+   */
+  keyExtractor?: ProviderComponent<K, T>;
+  /**
+   * The provider component used to extract values from events.
+   * If not provided, defaults to a provider that returns the event itself as the value.
+   */
+  valueExtractor?: ProviderComponent<V, T>;
+};
+
+/**
+ * MutatorHandler is an EventHandler that uses a Mutator to set values based on
+ * extracted keys and values from incoming events. Usually used to update state or
+ * save data to a store.
+ *
+ * @template T - The type of the Event being processed.
+ * @template K - The type of the key used by the Mutator.
+ * @template V - The type of the value being set by the Mutator.
+ */
+export class MutatorHandler<T extends Event = Event, K = string, V = T>
+  extends AbstractEventService
+  implements EventHandler<T>
+{
+  #mutator: MutatorFn<V, K>;
+  #keyExtractor: ProviderFn<K, T>;
+  #valueExtractor: ProviderFn<V, T>;
+
+  constructor(opts: MutatorHandlerOptions<T, K, V>) {
+    super(opts);
+
+    this.#mutator = getComponent(opts.mutator, 'set');
+    this.#keyExtractor = getComponent(opts.keyExtractor, 'provide', {
+      default: (event: T) => event.id as unknown as K,
+    });
+    this.#valueExtractor = getComponent(opts.valueExtractor, 'provide', {
+      default: (event: T) => event as unknown as V,
+    });
+  }
+
+  async handle(event: T): Promise<void> {
+    this.emit('event:received', event);
+    try {
+      const key = await this.#keyExtractor(event);
+      const value = await this.#valueExtractor(event);
+      await this.#mutator(key, value);
+      this.emit('event:processed', event);
+    } catch (error) {
+      this.emit('event:error', event, error);
+      throw error;
+    }
+  }
+}
