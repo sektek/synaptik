b74bcab95f340702d2571292be86d98183fd6614
Edward Kelly <eddie@sektek.net>
2026-01-17 10:18:57 -0800

    feat: TaskExecutionHandler (#29)

---
+++ src/handlers/task-execution-handler.ts
@@ -0,0 +1,87 @@
+import {
+  CommandComponent,
+  CommandFn,
+  ProviderComponent,
+  ProviderFn,
+  getComponent,
+} from '@sektek/utility-belt';
+
+import { Event, EventHandler } from '../types/index.js';
+
+import {
+  AbstractEventService,
+  EventServiceOptions,
+} from '../abstract-event-service.js';
+
+/** Options for configuring the TaskExecutionHandler */
+export type TaskExecutionHandlerOptions<T, C> = EventServiceOptions & {
+  /**
+   * The task command component to be executed. If not provided,
+   * a taskProvider must be specified to supply the task component.
+   */
+  task?: CommandComponent<C>;
+  /**
+   * A provider component that supplies the task command component.
+   * If not provided, a task must be specified directly.
+   */
+  taskProvider?: ProviderComponent<CommandComponent<C>, T>;
+  /**
+   * The context to be passed to the task command. If C is void, context
+   * is undefined.
+   */
+  context?: C extends void ? undefined : C;
+  /**
+   * A provider component that supplies the context for the task command.
+   * If not provided, the static context (if any) will be used.
+   * Should not be provided if C is void.
+   */
+  contextProvider?: ProviderComponent<C, T>;
+};
+
+/**
+ * A handler that executes a task command in response to an event.
+ *
+ * @template T - The type of the Event being handled.
+ * @template C - The type of the context passed to the task command.
+ *    Defaults to void if no context is needed.
+ */
+export class TaskExecutionHandler<T extends Event = Event, C = void>
+  extends AbstractEventService
+  implements EventHandler<T>
+{
+  #taskProvider: ProviderFn<CommandComponent<C>, T>;
+  #contextProvider: ProviderFn<C, T>;
+
+  constructor(opts: TaskExecutionHandlerOptions<T, C>) {
+    super(opts);
+
+    if (!opts.task && !opts.taskProvider) {
+      throw new Error(
+        'Either task or taskProvider must be provided to TaskExecutionHandler',
+      );
+    }
+
+    this.#taskProvider = getComponent(opts.taskProvider, 'get', {
+      default: () => opts.task!,
+    });
+
+    this.#contextProvider = getComponent(opts.contextProvider, 'get', {
+      default: () => opts.context as unknown as C,
+    });
+  }
+
+  async handle(event: T): Promise<void> {
+    this.emit('event:received', event);
+    try {
+      const taskComponent = await this.#taskProvider(event);
+      const context = await this.#contextProvider(event);
+      const task: CommandFn<C> = getComponent(taskComponent, 'execute');
+
+      await task(context);
+      this.emit('event:processed', event);
+    } catch (error) {
+      this.emit('event:error', event, error);
+      throw error;
+    }
+  }
+}
