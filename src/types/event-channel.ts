7e0e219422e8b4d42fb8f4e2ea54f5a97664c805
Edward Kelly <eddie@sektek.net>
2026-03-16 04:57:47 +0000

    feat: Enhance EventChannelFn to include options parameter for event sending

--- src/types/event-channel.ts
+++ src/types/event-channel.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmittingService } from '@sektek/utility-belt';
 
-import { EventHandlerEvents, EventHandlerFn } from './event-handler.js';
 import { Event } from './event.js';
+import { EventHandlerEvents } from './event-handler.js';
 import { EventService } from './event-service.js';
 
 export const EVENT_DELIVERED = 'event:delivered';
+
+export type EventChannelSendOptions = Record<string, unknown>;
 
 /**
  * An EventChannel Function is an event handler that specifically should
@@ -12,8 +14,16 @@ export const EVENT_DELIVERED = 'event:delivered';
  * outbound gateways for transporting events between systems.
  *
  * @template T - The event type that the channel can send.
+ *
+ * @param event - The event to send.
+ * @param options - Optional options for sending the event.
+ *
+ * @returns A promise that resolves when the event has been sent.
  */
-export type EventChannelFn<T extends Event = Event> = EventHandlerFn<T, void>;
+export type EventChannelFn<T extends Event = Event> = (
+  event: T,
+  options?: EventChannelSendOptions,
+) => void | Promise<void>;
 
 export type EventChannelEvents<T extends Event = Event> = EventHandlerEvents<
   T,
