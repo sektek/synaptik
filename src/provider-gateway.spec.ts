07ab7c3ae542df75a81fedbae39d4e56adfa50dc
Edward Kelly <eddie@sektek.net>
2026-03-16 05:01:31 +0000

    WIP

---
+++ src/provider-gateway.spec.ts
@@ -0,0 +1,11 @@
+import { expect, use } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import { ProviderGateway } from './provider-gateway.js';
+
+use(sinonChai);
+
+describe('ProviderGateway', function () {
+
+});
