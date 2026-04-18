b8bf2d3885d91a2dc1142fba11ad8874d43ecdf9
Edward Kelly <eddie@sektek.net>
2026-04-18 17:34:52 +0000

    fix(SEK-6): correct ExecutionStrategy usage in EventRouter
    Switch from utility-belt's generic ExecutionStrategy to the event-router's
    own typed variant, fix argument order in send(), and use ParallelStrategy
    as the default instead of parallelExecutionStrategy.

--- src/event-router/event-router.ts
+++ src/event-router/event-router.ts
@@ -1,21 +1,23 @@
-import {
-  ExecutionStrategyComponent,
-  ExecutionStrategyFn,
-  getComponent,
-  parallelExecutionStrategy,
-} from '@sektek/utility-belt';
+import { getComponent } from '@sektek/utility-belt';
 
 import {
   AbstractEventService,
   EventServiceOptions,
 } from '../abstract-event-service.js';
 import { Event, EventChannel } from '../types/index.js';
-import { RouteFn, RoutesProvider, RoutesProviderFn } from './types/index.js';
+import {
+  ExecutionStrategyComponent,
+  ExecutionStrategyFn,
+  RouteFn,
+  RoutesProvider,
+  RoutesProviderFn,
+} from './types/index.js';
+import { ParallelStrategy } from './execution-strategies/index.js';
 
 export type EventRouterOptions<T extends Event = Event> =
   EventServiceOptions & {
     routesProvider: RoutesProvider<T> | RoutesProviderFn<T>;
-    executionStrategy?: ExecutionStrategyComponent<RouteFn<T>>;
+    executionStrategy?: ExecutionStrategyComponent<T>;
   };
 
 /**
@@ -27,7 +29,7 @@ export class EventRouter<T extends Event = Event>
   implements EventChannel<T>
 {
   #routesProvider: RoutesProviderFn<T>;
-  #executionStrategy: ExecutionStrategyFn<RouteFn<T>>;
+  #executionStrategy: ExecutionStrategyFn<T>;
 
   constructor(options: EventRouterOptions<T>) {
     super(options);
@@ -38,7 +40,7 @@ export class EventRouter<T extends Event = Event>
       'execute',
       {
         name: 'executionStrategy',
-        default: parallelExecutionStrategy,
+        default: new ParallelStrategy<T>(),
       },
     );
   }
@@ -49,7 +51,7 @@ export class EventRouter<T extends Event = Event>
     for await (const route of await this.#routesProvider(event)) {
       routes.push(route);
     }
-    await this.#executionStrategy(routes, event);
+    await this.#executionStrategy(event, routes);
     this.emit('event:delivered', event);
   }
 }
